59c8f5d3047f604b78d5a775ff578dc2c93ae304
Carol <carol.li1016@gmail.com>
2021-03-16 22:05:40 +0800

    Add music_controller project

---
+++ practice/automatetheboringstuff/6hours/machine_learning.py
@@ -0,0 +1,8 @@
+# import the Data
+# clean the Data
+# Split the Data into Training/Test Sets.
+# Create a Model
+# Train the Model
+# Make the Predictions
+# Evaluate and Improve
+### Library: Numpy, Pandas, MatPlotLib, Scikit-Learn
